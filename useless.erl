6e58138d50970c4416fb42b2d58f49aa317f8e07
David Asabina <david@supr.nu>
2015-12-21 01:00:30 +0100

    attr 'export' before func defs

--- useless.erl
+++ useless.erl
@@ -1,4 +1,6 @@
 -module(useless).
+-export([add/2, greet_and_add_two/1,  hello/0]).
+
 %% Add B to A
 add(A, B) ->
   A + B.
@@ -9,4 +11,3 @@ greet_and_add_two(X) ->
   hello(),
   add(X,2).
 % exports
--export([add/2, hello/0]).

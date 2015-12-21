6096be3be54a235fa67c4201ae223034ac125df9
David Asabina <david@supr.nu>
2015-12-22 00:07:16 +0100

    More play with guard clauses

--- guards.erl
+++ guards.erl
@@ -33,3 +33,31 @@ old_enough(_) -> false.
 %% remember that `andalso` and `orelse` are short-circuit notations that only
 %% evaluate the the right-hand operand if necessary which means less
 %% computation in some cases.
+
+%% Experimenting with errors. Apparently using the `,` and `;` operators in
+%% guard clauses catches errors, so let's write a function that would allow for
+%% errors to be thrown in clauses (e.g.: additions between different types).
+valid_denominator_even_sum(X, Y) when X+Y > 0, X>0, Y>0 ->
+  X+Y;
+valid_denominator_even_sum(_, _) ->
+  false.
+
+% Add some does something whenever one of the operands is greater than zero.
+% This function should fail whenever one of the operands is not a number
+% ∴ not addable by one. In case X isn't a number the entire guard fails.
+% If X passes, the entire guard passes since the two operands are simply
+% subjected to the OR operation.
+add_nonzeros(X,Y) when X+1>=0 orelse Y+1>=0 ->
+  [X, Y];
+add_nonzeros(_,_) ->
+  fuckoff.
+% `add_nonzeros(atom, 12)` should fail while
+% `add_nonzeros_again(atom, 12)` should return `[atom, 12]`
+
+% Theoretically with X as a non-number (e.g.: an atom) the first guard
+% clause should fail, but the second one should pass resulting to the function
+% clause being handled.
+add_nonzeros_again(X,Y) when X+1>=1; Y+1>=1 ->
+  [X, Y];
+add_nonzeros_again(_,_) ->
+  fuckoff.

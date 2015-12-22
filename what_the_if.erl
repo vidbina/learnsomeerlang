27e06707b5c3f6cd378238d4864ffc8573591706
David Asabina <david@supr.nu>
2015-12-22 01:13:32 +0100

    if else

--- what_the_if.erl
+++ what_the_if.erl
@@ -1,5 +1,5 @@
 -module(what_the_if).
--export([heh_fine/0, bloo/0, evenness/1]).
+-export([heh_fine/0, bloo/0, evenness/1, help_me/1]).
 
 heh_fine() ->
   if 1 =:= 1 ->
@@ -11,9 +11,29 @@ heh_fine() ->
   if 1 =:= 2, 1 =:= 1 -> fails; true -> something
   end.
 
+% Erlang bitches whenever a guard definitely evaluates to false even if there
+% is a true branch, since it is already known that the true branch is
+% unreachable. Probably should have called this test function
+% `unreachable_truth` :P.
+problem_child() ->
+  if false -> problem; true -> no_problem end.
+
 evenness(N) ->
-  if (N rem 2) =:= 0 -> even; true -> uneven end.
+  if (N rem 2) =:= 0 ->
+       even;
+     true ->
+       uneven
+  end.
 
 bloo() ->
   bloo,
   blah.
+
+help_me(Animal) ->
+  Talk = if Animal == cat -> "meow";
+            Animal == beef -> "mooo";
+            Animal == dog -> "bark";
+            Animal == tree -> "bark";
+            true -> "bliepbliepbloop"
+         end,
+  { Animal, "says " ++ Talk ++ "!" }.

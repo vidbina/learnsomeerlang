f3430f2f9c592c4483a79b373fcb76676495be10
David Asabina <david@supr.nu>
2015-12-23 09:35:37 +0100

    tail recursion :snake:

--- recursive.erl
+++ recursive.erl
@@ -1,5 +1,35 @@
 -module(recursive).
--export([fac/1]).
+-export([fac/1, tail_fac/1, len/1, tail_len/1]).
 
+% n! = n * (n-1) * ... * 1
 fac(N) when N == 0 -> 1;
 fac(N) when N >= 1 -> N*fac(N-1).
+
+% n! = n * ( n-1 * ( ... ) )
+% n! = n * (n-1)!
+%   3! = 3*1, 2!
+%   2! = 2*(3*1), 1!
+%   1! = 1*(2*(3*1)), 0!
+%   f_o(3) -> f(3,1)
+%   f(3,1) -> f(2,3*1)
+%   f(2,3*1) -> f(1,2*3*1)
+%   f(0,2*3*1) -> (1*2*3) -> 6
+%
+tail_fac(N) -> tail_fac(N, 1).
+tail_fac(0, Acc) -> Acc;
+tail_fac(N, Acc) when N > 0 -> tail_fac(N-1, N*Acc).
+
+len([]) -> 0;
+len([_]) -> 1;
+len([_|T]) -> 1+len(T).
+
+% tail recursion: keep ops in args rather than function body in order to
+% circumvent long operations. Solve as you go.
+%
+% f_o([_,_,_,_]) -> f([_,_,_,_], 1)
+% f([_,_,_,_], 1) -> f([_,_,_], 1+1)
+% f([_,_,_], 1+1) -> f([_,_], 1+1+1)
+% f([_,_], 1+1+1) -> f([_], 1+1+1+1) -> 4
+tail_len(L) -> tail_len(L, 1).
+tail_len([_], Acc) -> Acc;
+tail_len([_|T], Acc) -> tail_len(T, 1+Acc).

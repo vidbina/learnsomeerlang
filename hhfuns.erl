04593be6df1cd8751da887efb1a760431791a20d
David Asabina <david@supr.nu>
2015-12-27 17:04:40 +0100

    anon functios while Anakin became Darth Vader

--- hhfuns.erl
+++ hhfuns.erl
@@ -6,3 +6,18 @@ two() -> 2.
 
 % Use as add(fun one/0, fun two/0)
 add(X, Y) -> X() + Y().
+
+increment([]) -> [];
+increment([H|T]) -> [H+1|increment(T)].
+
+decrement([]) -> [];
+decrement([H|T]) -> [H-1|decrement(T)].
+
+double(Val) -> 2*Val.
+
+% try hhfuns:map(fun hhfuns:double, [5,10,11]).
+map(_, []) -> [];
+map(F, [H|T]) -> [F(H)|map(F, T)].
+
+% example of directly calling an anon func
+% (fun(1) -> one; (2) -> two; (_) -> something end)(3).

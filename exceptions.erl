fbeec09db9c567952305d4147ea51a9871148e98
David Asabina <david@supr.nu>
2016-01-02 11:59:56 +0100

    catching different errors

--- exceptions.erl
+++ exceptions.erl
@@ -11,3 +11,22 @@ throws(F) ->
   catch
     Throw -> { throw, caught, Throw }
   end.
+
+% catcher(12, 2)
+% catcher(12, 0)
+catcher(X, Y) ->
+  case catch X/Y of
+    { 'EXIT', { badarith, _ }} -> 'your math game sucks';
+    N -> N
+  end.
+
+handle(F) ->
+  try F() of
+    _ -> ok
+  catch
+    error:badarith -> { error, caught, 'suck at math' };
+    error:Reason -> { error, caught, Reason };
+    throw:Reason -> { throw, caught, Reason };
+    exit:Reason -> { exit, caught, Reason }
+  end.
+

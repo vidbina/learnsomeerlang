2e6748beadadb9574af1dea3f5ac6931d58207d7
David Asabina <david@supr.nu>
2016-01-06 12:23:11 +0100

    read float and int from tokens

--- calc.erl
+++ calc.erl
@@ -19,12 +19,30 @@ calc(["*"|Tokens], [Multiplier|[Multiplicant|Stack]]) ->
 calc(["/"|Tokens], [Divisor|[Dividend|Stack]]) ->
   calc(Tokens, [Dividend/Divisor|Stack]);
 calc([Val|Tokens], Stack) ->
-  calc(Tokens, [erlang:list_to_integer(Val)|Stack]).
+  calc(Tokens, [read(Val)|Stack]).
 
+% fold(fun rpn/2, [], ["1", "3", "+", "2", "/"])
+% fold(rpn("1", []) -> [1], ["3", "+", "2", "/"])
+% fold(rpn("3", [1]) -> [3, 1], ["+", "2", "/"])
+% fold(rpn("+", [3, 1]) -> [4], ["2", "/"])
+% fold(rpn("2", [4]) -> [2, 4], ["/"])
+% fold(rpn("/", [2, 4]) -> [2], [])
 rpn(L) when is_list(L) ->
   [Res] = lists:foldl(fun rpn/2, [], string:tokens(L, " ")), Res.
 rpn("+", [A|[B|Rest]]) -> [A+B|Rest];
 rpn("-", [Subtrahend|[Minuend|Rest]]) -> [Minuend-Subtrahend|Rest];
 rpn("*", [Multiplier|[Multiplicant|Rest]]) -> [Multiplicant*Multiplier|Rest];
 rpn("/", [Divisor|[Dividend|Rest]]) -> [Dividend/Divisor|Rest];
-rpn(Operand, Acc) -> [erlang:list_to_integer(Operand)|Acc].
+rpn(Operand, Acc) -> [read(Operand)|Acc].
+
+list_to_number(N) ->
+  case string:to_float(N) of
+    { error, no_float } -> { Int, _ } = string:to_integer(N), Int;
+    { Float, _ } -> Float
+  end.
+
+read(N) ->
+  case string:to_float(N) of
+    { error, no_float } -> list_to_integer(N);
+    { F, _ } -> F
+  end.

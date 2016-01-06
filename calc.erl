6478c7348e4a470d8e37045e818a88add0aca6e8
David Asabina <david@supr.nu>
2016-01-06 13:59:55 +0100

    rpn test

--- calc.erl
+++ calc.erl
@@ -33,7 +33,34 @@ rpn("+", [A|[B|Rest]]) -> [A+B|Rest];
 rpn("-", [Subtrahend|[Minuend|Rest]]) -> [Minuend-Subtrahend|Rest];
 rpn("*", [Multiplier|[Multiplicant|Rest]]) -> [Multiplicant*Multiplier|Rest];
 rpn("/", [Divisor|[Dividend|Rest]]) -> [Dividend/Divisor|Rest];
+rpn("ln", [Operand|Rest]) -> [math:log(Operand)|Rest];
+rpn("log10", [Operand|Rest]) -> [math:log10(Operand)|Rest];
+rpn("sqrt", [Operand|Rest]) -> [math:sqrt(Operand)|Rest];
+rpn("^", [Exponent|[Base|Rest]]) -> [math:pow(Base, Exponent)|Rest];
+rpn("e", Rest) -> [math:exp(1)|Rest];
+rpn("pi", Rest) -> [math:pi()|Rest];
 rpn(Operand, Acc) -> [read(Operand)|Acc].
+
+rpn_test() ->
+  5 = rpn("2 3 +"),
+  87 = rpn("90 3 -"),
+  -4 = rpn("10 4 3 + 2 * -"),
+  -2.0 = rpn("10 4 3 + 2 * - 2 /"),
+  fails = try
+           rpn("90 34 12 33 55 66 + * - +")
+         catch
+           error:{badmatch,[_|_]} -> fails
+          end,
+  4037 = rpn("90 34 12 33 55 66 + * - + -"),
+  8.0 = rpn("2 3 ^"),
+  true = math:sqrt(2) == rpn("2 0.5 ^"),
+  true = math:sqrt(10) == rpn("10 sqrt"),
+  true = math:log(2.7) == rpn("2.7 ln"),
+  true = math:log10(2.7) == rpn("2.7 log10"),
+%  50 = rpn("10 10 10 10 sum"),
+%  10.0 = rpn("10 10 10 20 sum 5 /"),
+%  1000.0 = rpn("10 10 20 0.5 prod"),
+  ok.
 
 list_to_number(N) ->
   case string:to_float(N) of

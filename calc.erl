d682d5b3adc48f33c46a224fa8e575d8864893ae
David Asabina <david@supr.nu>
2016-01-06 02:06:50 +0100

    Folding rpn

--- calc.erl
+++ calc.erl
@@ -4,8 +4,21 @@
 calc(Expr) -> calc(string:tokens(Expr, " "), []).
 
 calc([], [Val|_]) -> Val;
-calc(["+"|Tokens], [L|[R|Stack]]) -> calc(Tokens, [L+R|Stack]);
-calc(["-"|Tokens], [Subtrahend|[Minuend|Stack]]) -> calc(Tokens, [Minuend-Subtrahend|Stack]);
-calc(["*"|Tokens], [Multiplier|[Multiplicant|Stack]]) -> calc(Tokens, [Multiplicant*Multiplier|Stack]);
-calc(["/"|Tokens], [Divisor|[Dividend|Stack]]) -> calc(Tokens, [Dividend/Divisor|Stack]);
-calc([Val|Tokens], Stack) -> calc(Tokens, [erlang:list_to_integer(Val)|Stack]).
+calc(["+"|Tokens], [L|[R|Stack]]) ->
+  calc(Tokens, [L+R|Stack]);
+calc(["-"|Tokens], [Subtrahend|[Minuend|Stack]]) ->
+  calc(Tokens, [Minuend-Subtrahend|Stack]);
+calc(["*"|Tokens], [Multiplier|[Multiplicant|Stack]]) ->
+  calc(Tokens, [Multiplicant*Multiplier|Stack]);
+calc(["/"|Tokens], [Divisor|[Dividend|Stack]]) ->
+  calc(Tokens, [Dividend/Divisor|Stack]);
+calc([Val|Tokens], Stack) ->
+  calc(Tokens, [erlang:list_to_integer(Val)|Stack]).
+
+rpn(L) when is_list(L) ->
+  [Res] = lists:foldl(fun rpn/2, [], string:tokens(L, " ")), Res.
+rpn("+", [A|[B|Rest]]) -> [A+B|Rest];
+rpn("-", [Subtrahend|[Minuend|Rest]]) -> [Minuend-Subtrahend|Rest];
+rpn("*", [Multiplier|[Multiplicant|Rest]]) -> [Multiplicant*Multiplier|Rest];
+rpn("/", [Divisor|[Dividend|Rest]]) -> [Dividend/Divisor|Rest];
+rpn(Operand, Acc) -> [erlang:list_to_integer(Operand)|Acc].

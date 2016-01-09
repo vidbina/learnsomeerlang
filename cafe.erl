7cfc819f1d73cf68f4b060e4c20f921a8302ebd1
David Asabina <david@supr.nu>
2016-01-09 18:38:13 +0100

    saymore/2 experiment
    Trying to run multiple jobs in a single process trying to discover if
    the execute sequentially and if the order is changed. Seems like the
    order is maintained in some of my experiments, however; it really sucks
    since I'm doing all the waiting as well. I was hoping that mapping all
    transmissions would send them into the ether after which I could pick
    everything up quickly as soon as I'm ready.

---
+++ cafe.erl
@@ -0,0 +1,40 @@
+-module(msg).
+-export([start/0, say/2, saymore/2, cafe/0]).
+
+cafe() ->
+  receive
+    { Pid, hello } ->
+      timer:sleep(3000),
+      Pid ! { self(), hey },
+      cafe();
+    { Pid, marco } ->
+      timer:sleep(1000),
+      Pid ! { self(), polo },
+      cafe();
+    { Pid, polo } ->
+      timer:sleep(1000),
+      Pid ! { self(), marco },
+      cafe();
+    { Pid, bye } -> 
+      timer:sleep(1000),
+      Pid ! { self(), adios };
+    { Pid, Something } ->
+      timer:sleep(2000),
+      io:format("~p~n", [Something]),
+      Pid ! { self(), Something },
+      cafe()
+  end.
+  
+start() -> spawn(?MODULE, cafe, []).
+say(Pid, Something) ->
+  Pid ! { self(), Something },
+  receive
+    { _, Msg } ->
+      io:format("~p => ~p~n", [Something, Msg]),
+      Msg
+  end.
+saymore(Pid, Things) ->
+  lists:map(fun(Something) -> Pid ! { self(), Something } end, Things),
+  receive
+    { _, Msg } -> io:format("got ~p~n", [Msg])
+  end.

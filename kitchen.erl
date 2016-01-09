a2c2a38a6238f240b8a4ba3dd73ae62496736cb9
David Asabina <david@supr.nu>
2016-01-09 15:18:08 +0100

    own implementation of state-ful fridge

---
+++ kitchen.erl
@@ -0,0 +1,30 @@
+-module(kitchen).
+-compile(export_all).
+
+fridge1() ->
+  receive
+    { From, { store, _Food } } ->
+      From ! { self(), ok },
+      fridge1();
+    { From, { take, _Food } } ->
+      From ! { self(), ok },
+      fridge1();
+    terminate -> ok
+  end.
+
+fridge() -> fridge([]).
+fridge(Content) ->
+  receive
+    { From, { store, Food } } ->
+      From ! { self(), ok },
+      fridge([Food|Content]);
+    { From, { take, Food } } ->
+      case lists:any(fun(X) -> Food==X end, Content) of
+        true -> From ! { self(), ok }, fridge(lists:delete(Food, Content));
+        false -> From ! { self(), out }, fridge(Content)
+      end;
+    { _, { view } } ->
+      io:format("~p~n", [Content]),
+      fridge(Content);
+    terminate -> ok
+  end.

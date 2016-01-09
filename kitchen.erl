bda61ad56235656ed6ae98a48298a0ce4155d136
David Asabina <david@supr.nu>
2016-01-09 16:32:19 +0100

    hidden the msg internals

--- kitchen.erl
+++ kitchen.erl
@@ -11,15 +11,28 @@ fridge(Content) ->
     { From, { take, Food } } ->
       case lists:member(Food, Content) of
         true -> From ! { self(), ok }, fridge(lists:delete(Food, Content));
-        false -> From ! { self(), out }, fridge(Content)
+        false -> From ! { self(), not_found }, fridge(Content)
       end;
-    { _, { view } } ->
-      io:format("~p~n", [Content]),
+    { From, { view } } ->
+      From ! { content, Content },
       fridge(Content);
     terminate -> ok
   end.
 
-start(Contents) -> spawn(?MODULE, fridge, [Contents]).
-store(Pid, Item) -> Pid ! { self(), { store, Item } }.
-take(Pid, Item) -> Pid ! { self(), { take, Item } }.
-view(Pid) -> Pid ! { self(), { view } }.
+start(Contents) ->
+  spawn(?MODULE, fridge, [Contents]).
+store(Pid, Item) ->
+  Pid ! { self(), { store, Item } },
+  receive
+    { _, Msg } -> Msg
+  end.
+take(Pid, Item) ->
+  Pid ! { self(), { take, Item } },
+  receive
+    { _, Msg } -> Msg
+  end.
+view(Pid) ->
+  Pid ! { self(), { view } },
+  receive
+    { content, List } -> List
+  end.

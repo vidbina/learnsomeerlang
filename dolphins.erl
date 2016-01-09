f64518e571f0ab92e15befb94620da3ef48a1779
David Asabina <david@supr.nu>
2016-01-09 14:31:10 +0100

    gave :dolphin: more attitude

--- dolphins.erl
+++ dolphins.erl
@@ -7,3 +7,25 @@ dolphin1() ->
     fish -> io:format("thank you, buddy~n");
     _ -> io:format("doing the sonar thing~n")
   end.
+
+dolphin2() ->
+  receive
+    { Sender, flip } -> Sender ! "Do one yourself";
+    { Sender, fish } -> Sender ! "Thank you, buddy";
+    _ -> sonar 
+  end.
+
+% Dolphin = spawn(dolphins, dolphin, []).
+% Dolphin ! flip.
+% Dolphin ! blah.
+% Dolphin ! flip.
+% Dolphin ! fish.
+% Dolphin ! flip.
+% flush()
+dolphin3() ->
+  receive
+    { Sender, flip } -> Sender ! "Do one yourself", dolphin3();
+    { Sender, fish } -> Sender ! "Thank you, buddy", dolphin3();
+    { Sender, choo } -> Sender ! "Whatever";
+    _ -> sonar, dolphin3() 
+  end.

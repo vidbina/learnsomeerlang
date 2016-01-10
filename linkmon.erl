d2d3a7a7a458ef446b6b5c99a33e37b16862a756
David Asabina <david@supr.nu>
2016-01-10 02:04:46 +0100

    trapping exits :fish:

--- linkmon.erl
+++ linkmon.erl
@@ -4,6 +4,27 @@
 myproc() -> timer:sleep(5000), io:format("yawn"), exit(reason).
 ok() -> timer:sleep(5000), io:format("done").
 
+friendlychainreceives(N) ->
+  receive
+    die -> exit(fire);
+    bye -> adios;
+    _ -> ok, io:format("~p got the memo~n", [N]), friendlychainreceives(N)
+  after 10000 -> exit(fuckit)
+  end.
+
+% process_flag(trap_exit, true),
+% spawn_link(fun() -> linkmon:friendlychain(10) end),
+% receive X -> X end.
+friendlychain(N) ->
+  case N of
+    0 -> io:format("terminal at ~p~n", [self()]);
+    _ ->
+      Pid = spawn(?MODULE, friendlychain, [N-1]),
+      io:format("unit with N=~p at ~p~n",[N, Pid]),
+      link(Pid)
+  end,
+  friendlychainreceives(N).
+
 % do a test with bidirectional links
 ying() ->
   receive

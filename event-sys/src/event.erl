2730b338991a9f119d7d591e2ee9f3722b40ca4a
David Asabina <david@supr.nu>
2016-01-11 01:29:35 +0100

    split time into manageable slots :clock:
    Apparently one can only use delays as large as 365*24*60*60*1000. In
    order to respect that limitation we split up the delay into periods that
    are small enough to fall within the limit.

--- event-sys/src/event.erl
+++ event-sys/src/event.erl
@@ -1,19 +1,32 @@
 -module(event).
--export([loop/1, start/3]).
+-export([loop/1, start/3, start/4, normalize/2]).
 -record(state, { server, name="", to_go=0 }).
 
-start(Server, Name, Seconds) ->
-  Pid = spawn(fun() -> loop(#state{server=Server, name=Name, to_go=Seconds}) end),
+% time in milliseconds
+start(Server, Name, Time) -> start(Server, Name, Time, 1000).
+start(Server, Name, Time, Period) -> 
+  Pid = spawn(fun() -> loop(#state{
+                          server=Server,
+                          name=Name,
+                          to_go=normalize(Time, Period)}
+                      ) end),
   io:format("Registering ~p on ~p", [Pid, self()]),
   register(Name, Pid).
 
-loop(State = #state{server=Server}) ->
+loop(State = #state{server=Server, to_go=[T|Next]}) ->
   receive
     { Server, Ref, cancel } ->
       io:format("Cancel ~p~n", [State#state.name]),
       Server ! { Ref, ok }
-  after
-    State#state.to_go*1000 ->
-      io:format("Completed ~p after ~ps~n", [State#state.name, State#state.to_go]),
-      Server ! { done, State#state.name }
+  after T ->
+          if Next == [] ->
+               io:format("Completed ~p~n", [State#state.name]),
+               Server ! { done, State#state.name };
+             true ->
+               io:format("Extend ~p~n", [State#state.name]),
+               loop(State#state{to_go=Next})
+          end
   end.
+
+normalize(Duration, Limit) ->
+  [Duration rem Limit|lists:duplicate(Duration div Limit, Limit)].

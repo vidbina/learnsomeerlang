9db8cf9743a6f96d2806630c074bd427a77fe20a
David Asabina <david@supr.nu>
2016-01-18 18:34:12 +0100

    testing supervised supervisors

--- servers/super.erl
+++ servers/super.erl
@@ -10,7 +10,7 @@ init(Args=#{supervisor:=SupervisorStrategy, children:=ChildSpecs}) ->
 
 %% helpers
 start(basic, SupervisorRef) ->
-  io:format("starting supervisor ~p~n", [SupervisorRef]),
+  io:format("starting supervisor basic ~p~n", [SupervisorRef]),
   supervisor:start_link({local, SupervisorRef},
                         ?MODULE,
                         #{supervisor => #{
@@ -31,4 +31,22 @@ start(basic, SupervisorRef) ->
                                          start => {basic, start, [900]},
                                          restart => permanent, %% transient,
                                          shutdown => 3000,
-                                         type => worker } ] }).
+                                         type => worker } ] });
+start(deep, SupervisorRef) ->
+  io:format("starting supervisor deep ~p~n", [SupervisorRef]),
+  supervisor:start_link({local, SupervisorRef},
+                        ?MODULE,
+                        #{supervisor => #{
+                            strategy => one_for_one,
+                            intensity => 3,
+                            period => 5000 },
+                          children => [#{id => kylo,
+                                         start => {basic, start, [0]},
+                                         restart => permanent,
+                                         shutdown => 3000,
+                                         type => worker },
+                                       #{id => yoda,
+                                         start => {?MODULE, start, [basic, o]},
+                                         restart => permanent,
+                                         shutdown => 3000,
+                                         type => supervisor} ] }).
--- servers/test_super.erl
+++ servers/test_super.erl
@@ -10,7 +10,8 @@ run() -> run(all).
 run(all) ->
   run(exit_normal_does_not_terminate_children),
   run(exit_kill_does_terminate_children),
-  run(exit_other_does_not_terminate_children);
+  run(exit_other_does_not_terminate_children),
+  run(exit_supervising_child);
 run(exit_normal_does_not_terminate_children) ->
   io:format("testing as ~p~n", [self()]),
   %% test killing all tasks multiple times and watch if they restart
@@ -46,8 +47,8 @@ run(exit_kill_does_terminate_children) ->
 run(exit_other_does_not_terminate_children) ->
   io:format("testing as ~p~n", [self()]),
   %% test killing all tasks multiple times and watch if they restart
-  {ok, Supervisor} = super:start(basic, test_two),
-  Children = supervisor_children_pids(test_two),
+  {ok, Supervisor} = super:start(basic, test_other),
+  Children = supervisor_children_pids(test_other),
   % kill every child using exit(Pid, kill)
   io:format("children@0 ~p~n", [Children]),
   [exit(Child, other) || Child <- Children, is_process_alive(Child)],
@@ -55,7 +56,34 @@ run(exit_other_does_not_terminate_children) ->
     Msg -> io:format("got ~p something back~n", [Msg])
   after 2000 -> io:format("waited too long~n")
   end,
-  io:format("chidren@1 ~p~n", [supervisor_children_pids(test_two)]),
-  true = (supervisor_children_pids(test_two) == Children),
+  io:format("chidren@1 ~p~n", [supervisor_children_pids(test_other)]),
+  true = (supervisor_children_pids(test_other) == Children),
+  exit(Supervisor, normal),
+  passed;
+run(exit_supervising_child) ->
+  io:format("testing as ~p~n", [self()]),
+  %% test killing all tasks multiple times and watch if they restart
+  {ok, Supervisor} = super:start(deep, tree),
+  Children = supervisor_children_pids(tree),
+  % kill every child using exit(Pid, kill)
+  %io:format("~p~n", [C || C <- supervisor:which_children(tree)]),
+  Sups = [S || S<-supervisor:which_children(tree), element(3, S)==supervisor],
+  io:format("children ~p~n", [supervisor:which_children(tree)]),
+  receive
+  after 2000 -> ChildSupChildren = supervisor:which_children(o)
+  end,
+  [exit(Child, kill) || Child <- Children, is_process_alive(Child)],
+  receive
+    Msg -> io:format("got ~p something back~n", [Msg])
+  after 2000 -> io:format("waited too long~n"),
+                io:format("sup kids~p~n", [ChildSupChildren])
+  end,
+  io:format("sups ~p~n", [Sups]),
+  io:format("children ~p~n", [supervisor:which_children(tree)]),
+  io:format("children@0 ~p~n", [Children]),
+  io:format("children@1 ~p~n", [supervisor_children_pids(tree)]),
+  io:format("yoda children@0 ~p~n", [ChildSupChildren]),
+  io:format("yoda children@1 ~p~n", [supervisor:which_children(o)]),
+  false = (supervisor_children_pids(tree) == Children),
   exit(Supervisor, normal),
   passed.
